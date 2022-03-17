f4369a70723b6e2be1810784d8214c1d0f63bfe7
rppanya <100025463+rppanya@users.noreply.github.com>
2022-03-17 17:05:21 +0700

    A* algorithm 1

--- task1/1.js
+++ task1/1.js
@@ -1,7 +1,7 @@
 const elem = document.querySelector('#table');
 
 function createTable(parent, n) {
-    var fc = parent.firstChild;
+    let fc = parent.firstChild;
     while(fc) {
         parent.removeChild(fc);
         fc = parent.firstChild;
@@ -16,4 +16,54 @@ function createTable(parent, n) {
         table.appendChild(tr);
     }
     parent.appendChild(table);
+}
+
+//тут нужно написать функцию, расчитывающую h, например расстояние по прямой между двумя клетками
+
+
+//в эту функцию нужно будет передать массив map в виде матрицы смежности графа, построенного на основе карты
+// h - эвристическая оценка расстояния между точками(точно меньше фактического расстояния)
+const aStar = function (map, h, start, finish) {
+    let distances = []; //массив с расстояниями от старта до всех вершин
+    for (let i=0; i<map.length; i++) distances[i]=Number.MAX_VALUE;
+    distances[start] = 0;
+
+    let priorities = []; //массив с элементами, которые нужно посетить
+    for (var i=0; i<map.length; i++) priorities[i]=Number.MAX_VALUE;
+    priorities[start]=h[start][finish];
+
+    let visited = []; //массив с посещенными вершинами
+
+    let previous = []; //для каждой вершины будем хранить предыдущую в пути
+    previous[start]=-1;
+
+    while(true) {
+
+        //среди непосещенных узлов ищем узел с наибольшим приоритетом (наименьшим значением)
+        var lowestPriority = Number.MAX_VALUE;
+        var lowestPriorityIndex = -1;
+        for (let i=0; i<priorities.length; i++) {
+            if (priorities[i] < lowestPriority && !visited[i]) {
+                lowestPriority = priorities[i];
+                lowestPriorityIndex = i;
+            }
+        }
+        if (lowestPriorityIndex === -1) {
+            //все вершины посещены, пути нет
+            return -1;
+        } else if (lowestPriorityIndex === finish) {
+            return previous;
+        }
+
+        for (let i=0; i<map[lowestPriorityIndex].length; i++) {
+            if (map[lowestPriorityIndex][i] !== 0 && !visited[i]) {
+                if (distances[lowestPriorityIndex]+map[lowestPriorityIndex][i] < distances[i]) {
+                    distances[i] = distances[lowestPriorityIndex] + map[lowestPriorityIndex][i];
+                    priorities[i] = distances[i] + h[i][finish];
+                }
+                previous[i]=lowestPriorityIndex;
+            }
+        }
+        visited[lowestPriorityIndex]=true;
+    }
 }
