747990313fd762ee437536a650a7e6a5b6deef85
rppanya <100025463+rppanya@users.noreply.github.com>
2022-03-17 22:22:10 +0700

    A* page design 6(start and finish)

--- task1/1.js
+++ task1/1.js
@@ -11,6 +11,7 @@ function createTable(parent, n) {
         const tr = document.createElement('tr')
         for (let j=0; j<n; j++) {
             const td = document.createElement('td')
+            td.id = j+" "+i;
             table.appendChild(td);
             td.addEventListener("click",function(event){
                 barriers(td);
@@ -19,18 +20,38 @@ function createTable(parent, n) {
         table.appendChild(tr);
     }
     parent.appendChild(table);
-
 }
 
 function barriers(td) {
-    if (td.style.backgroundColor === "red") {
+
+    if (td.style.backgroundColor === "black") {
         td.style.backgroundColor = "";
     }
     else {
-        td.style.backgroundColor = "red";
+        td.style.backgroundColor = "black";
     }
 }
 
+function startPoint(x, y) {
+    let all=document.querySelectorAll('td');
+    for (let i=0; i<all.length; i++) {
+        if (all[i].style.backgroundColor==="green") {
+            all[i].style.backgroundColor="";
+        }
+    }
+    let p = document.getElementById((y-1)+" "+(x-1));
+    p.style.backgroundColor="green";
+}
+function finishPoint(x, y) {
+    let all=document.querySelectorAll('td');
+    for (let i=0; i<all.length; i++) {
+        if (all[i].style.backgroundColor==="red") {
+            all[i].style.backgroundColor="";
+        }
+    }
+    let p = document.getElementById((y-1)+" "+(x-1));
+    p.style.backgroundColor="red";
+}
 
 //тут нужно написать функцию, расчитывающую h, например расстояние по прямой между двумя клетками
 
