f44dc5650a2bd64e616c6617eb49659bf01bff72
rppanya <100025463+rppanya@users.noreply.github.com>
2022-03-19 00:40:20 +0700

    A* algorithm 2

--- task1/1.js
+++ task1/1.js
@@ -53,7 +53,7 @@ function finishPoint(x, y) {
     }
     let p = document.getElementById((y-1)+" "+(x-1));
     p.style.backgroundColor="red";
-
+    createMatrix()
 }
 
 //тут нужно написать функцию, расчитывающую h, например расстояние по прямой между двумя клетками
@@ -74,6 +74,99 @@ function createMatrix() {
             }
         }
     }
+    //console.log(matrix);
+    adjMatrix(matrix);
+
     return matrix;
 }
 
+function adjMatrix(matrix) {
+    let numberMatrix = matrix
+    for (let i=0; i<numberMatrix.length; i++) {
+        for (let j=0; j<numberMatrix.length; j++) {
+            numberMatrix[i][j]=i*numberMatrix.length+j
+        }
+    }
+
+    let adjMatrix = new Array(matrix.length*matrix.length);
+    for (let i=0; i<adjMatrix.length; i++) {
+        matrix[i]=new Array(matrix.length*matrix.length);
+    }
+    for (let i=0; i<adjMatrix.length; i++) {
+        for (let j=0; j<adjMatrix.length; j++) {
+            adjMatrix[i][j]=0
+        }
+    }
+    for (let i=0; i<matrix.length; i++) {
+        for (let j=0; j<matrix.length; j++) {
+            if (matrix[i][j+1]===1 && j<matrix.length-1) {
+                adjMatrix[numberMatrix[i][j]][numberMatrix[i][j+1]]=1;
+            }
+            if (matrix[i+1][j]===1 && i<matrix.length-1) {
+                adjMatrix[numberMatrix[i][j]][numberMatrix[i+1][j]]=1;
+            }
+            if (matrix[i-1][j]===1 && i>1) {
+                adjMatrix[numberMatrix[i][j]][numberMatrix[i-1][j]]=1;
+            }
+            if (matrix[i][j-1]===1 && j>1) {
+                adjMatrix[numberMatrix[i][j]][numberMatrix[i][j-1]]=1;
+            }
+        }
+    }
+    console.log(adjMatrix)
+}
+
+//тут нужно написать функцию, расчитывающую h, например расстояние по прямой между двумя клетками
+const heuristic = function (startX, startY, finishX, finishY) {
+    return Math.abs(startX - finishX) + Math.abs(startY - finishY);
+}
+
+
+//в эту функцию нужно будет передать массив map в виде матрицы смежности графа, построенного на основе карты
+// h - эвристическая оценка расстояния между точками(точно меньше фактического расстояния)
+
+const aStar = function (map, h, start, finish) {
+
+    let distances = []; //массив с расстояниями от старта до всех вершин
+    for (let i=0; i<map.length; i++) distances[i]=Number.MAX_VALUE;
+    distances[start] = 0;
+
+    let priorities = []; //массив с элементами, которые нужно посетить
+    for (var i=0; i<map.length; i++) priorities[i]=Number.MAX_VALUE;
+    priorities[start]=h[start][finish];
+
+    let visited = []; //массив с посещенными вершинами
+
+    let previous = []; //для каждой вершины будем хранить предыдущую в пути
+    previous[start]=-1;
+
+    while(true) {
+
+        //среди непосещенных узлов ищем узел с наибольшим приоритетом (наименьшим значением)
+        var lowestPriority = Number.MAX_VALUE;
+        var lowestPriorityIndex = -1;
+        for (let i=0; i<priorities.length; i++) {
+            if (priorities[i] < lowestPriority && !visited[i]) {
+                lowestPriority = priorities[i];
+                lowestPriorityIndex = i;
+            }
+        }
+        if (lowestPriorityIndex === -1) {
+            //все вершины посещены, пути нет
+            return -1;
+        } else if (lowestPriorityIndex === finish) {
+            return previous;
+        }
+
+        for (let i=0; i<map[lowestPriorityIndex].length; i++) {
+            if (map[lowestPriorityIndex][i] !== 0 && !visited[i]) {
+                if (distances[lowestPriorityIndex]+map[lowestPriorityIndex][i] < distances[i]) {
+                    distances[i] = distances[lowestPriorityIndex] + map[lowestPriorityIndex][i];
+                    priorities[i] = distances[i] + h[i][finish];
+                }
+                previous[i]=lowestPriorityIndex;
+            }
+        }
+        visited[lowestPriorityIndex]=true;
+    }
+}
