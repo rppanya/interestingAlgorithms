020416736c702fd6d5bc1d095a2b8bf3077f8eab
DanilGrasmik <dankagrasmik@gmail.com>
2022-03-29 16:51:55 +0700

    t2 design

--- task2/2.js
+++ task2/2.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext("2d");
-canvas.height = 693;
+canvas.height = 593;
 canvas.width = 700;
 let points = [];
 let cluster = [];
